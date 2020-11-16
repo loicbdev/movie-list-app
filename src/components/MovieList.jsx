8e77f8d1abf480fca2e53ffd10fb9475130b8466
loicbdev <loicbodolec@msn.com>
2020-11-16 12:13:44 +0100

    props pour ALIEN

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,10 @@
-import React, { Component }  from 'react';
-import Movie from './Movie';
-
+import React, { Component } from "react";
+import Movie from "./Movie";
 
 class MovieList extends Component {
-
-    render() {
-        return (
-            <Movie />
-        )};
+  render() {
+    return <Movie title="Alien" year="1979" director="Ridley Scott" />;
+  }
 }
 
 export default MovieList;
