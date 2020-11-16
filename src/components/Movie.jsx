3d6eaac5580a363b73189be8ca103d527d9824a9
loicbdev <loicbodolec@msn.com>
2020-11-16 15:42:16 +0100

    prop-types

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Movie = (props) => {
   return (
@@ -8,6 +9,12 @@ const Movie = (props) => {
       <h2>director : {props.director}</h2>
     </div>
   );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  director: PropTypes.string.isRequired
 }
 
 export default Movie;
