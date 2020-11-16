3ec94655b71fb70aef72683c3c22857d6b967125
loicbdev <loicbodolec@msn.com>
2020-11-16 11:53:54 +0100

    création des components

---
+++ src/components/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Movie = (props) => {
+  return (
+    <div className="Movie">
+      <h1>title : {props.title}</h1>
+      <h2>year : {props.year}</h2>
+      <h2>director : {props.director}</h2>
+    </div>
+  );
+};
+
+export default Movie;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Movie from './components/Movie';
+
+
+class MovieList extends React.Component {
+
+    render() {
+        return (
+            <Movie />
+        )};
+}
+
+export default MovieList;
