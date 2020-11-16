ce9add937da7d5af28077d72873637f0bb807b5e
loicbdev <loicbodolec@msn.com>
2020-11-16 18:47:37 +0100

    installation styled-components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NAVBAR = styled.div`
+  margin: 1rem;
+  font-size : 25px;
+  text-align: left;
+`;
 
 function NavBar() {
   return (
     <div>
-      <Link to="/movies">Movie List</Link>
+      <NAVBAR>
+        <Link to="/movies">Movie List</Link>
+      </NAVBAR>
     </div>
   );
 }
